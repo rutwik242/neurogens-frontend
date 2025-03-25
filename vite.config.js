e71d9d50923f2c769a29051b572dc2d81bec544b
Rutwik S. Nainoor <rutwikreddy242@gmail.com>
2025-03-25 05:40:40 +0530

    ✅ Fix base path and install axios

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// ✅ Change this to your GitHub repo name
 export default defineConfig({
-  plugins: [vue()]
+  base: "/neurogens-frontend/", // <-- Important!
+  plugins: [vue()],
 })
